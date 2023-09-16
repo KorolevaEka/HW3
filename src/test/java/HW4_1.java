fd0d06352acaf1539b3d6e9a11514ade616bea6d
Екатерина <katty.a.koroleva@gmail.com>
2023-09-16 21:35:19 +0200

    Testing Forms, delete unused imports

--- src/test/java/HW4_1.java
+++ src/test/java/HW4_1.java
@@ -14,7 +14,17 @@ public class HW4_1 {
         $x("//li[@class='Box-row wiki-more-pages-link']//button").click();
         $x("//div[@id='wiki-pages-box']").shouldHave(text("SoftAssertions"));
         $x("//span//a[@href=\"/selenide/selenide/wiki/SoftAssertions\"]").click();
-        $x("//div[@class='markdown-body']").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+        $x("//div[@class='markdown-body']").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
 
     }
